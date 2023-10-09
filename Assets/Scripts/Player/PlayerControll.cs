fb95a0e0a7b2b74ce549030549d7fbbe479b9f84
junkwon <ansdj1908@gmail.com>
2023-10-09 21:10:47 +0900

    [Feat] 플레이어의 대쉬 기능

--- Assets/Scripts/Player/PlayerControll.cs
+++ Assets/Scripts/Player/PlayerControll.cs
@@ -7,12 +7,17 @@ public class PlayerControll : MonoBehaviour
     public static PlayerControll Instance;
 
     [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private float dashSpeed = 4f;
+    [SerializeField] private TrailRenderer myTrail;
+
 
     private PlayerControlls playerControlls;
     private Vector2 movement;
     private Rigidbody2D rb;
     private Animator myAni;
     private SpriteRenderer mySprite;
+
+    private bool isDashing = false;
 
     private void Awake()
     {
@@ -22,7 +27,10 @@ public class PlayerControll : MonoBehaviour
         myAni = GetComponent<Animator>();
         mySprite = GetComponent<SpriteRenderer>();
     }
-
+    private void Start()
+    {
+        playerControlls.Combat.Dash.performed += _ => Dash();
+    }
     private void Update()
     {
         playerInput();
@@ -60,5 +68,27 @@ public class PlayerControll : MonoBehaviour
             mySprite.flipX = true;
         }
         else mySprite.flipX = false;
+    }
+
+    private void Dash()
+    {
+        if (!isDashing)
+        {
+        isDashing = true;
+        moveSpeed *= dashSpeed;
+        myTrail.emitting = true;
+        StartCoroutine(EndDashRoutine());
+        }
+    }
+
+    private IEnumerator EndDashRoutine()
+    {
+        float dashTime = .2f;
+        float dashCD = .25f;
+        yield return new WaitForSeconds(dashTime);
+        moveSpeed /= dashSpeed;
+        myTrail.emitting = false;
+        yield return new WaitForSeconds(dashCD);
+        isDashing = false;
     }
 }
